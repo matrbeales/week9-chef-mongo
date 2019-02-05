31adad3ef2f5d8116b6a34f4812aaacba3df2c93
unknown <AClark@spartaglobal.com>
2019-02-05 21:33:00 +0000

    Attempted to add mongodb

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,13 @@
 # Recipe:: default
 #
 # Copyright:: 2019, The Authors, All Rights Reserved.
+include_recipe 'apt'
+
+apt_repository 'mongodb' do
+  uri        'http://repo.mongodb.org/apt/ubuntu'
+  components ['mongodb']
+end
+
+package 'mongodb'
+
+# apt_update
